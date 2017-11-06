dfd21030d061047bb037b7975c847ec3735325bc
shaokr <shaokangrun@91kook.com>
2017-11-06 09:46:50 +0800

    创建基本目录

---
+++ src/js/util/debug-tool.js
@@ -0,0 +1,47 @@
+/**
+ * 开启调试: 点击5次并且按住5秒钟
+ */
+import Systemjs from 'systemjs';
+import param from 'util/param';
+import PromiseClass from 'util/promise-class';
+
+const debug = new PromiseClass();
+const debugTool = new PromiseClass();
+if (param.debug) {
+    debug.resolve();
+}
+debug.promise
+	.then(() => Systemjs.import('cc-debug-tool'))
+	.then(debugTool.resolve);
+let i = 0;
+let ct = 0;
+document.addEventListener('touchstart', () => {
+    clearTimeout(ct);
+    i++;
+    if (i <= 4) {
+        ct = setTimeout(() => {
+            i = 0;
+        }, 500);
+    } else {
+        ct = setTimeout(() => {
+            debug.resolve();
+        }, 5000);
+    }
+});
+
+document.addEventListener('touchend', () => {
+    if (i > 4) {
+        i = 0;
+        clearTimeout(ct);
+    }
+});
+
+function tool(key, data) {
+    debugTool.promise.then((Tool) => {
+        Tool(key, data);
+    });
+}
+
+export const log = data => tool('log', data);
+
+export default tool;
